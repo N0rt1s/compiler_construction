a4f3e31ecee9f3a2188bf5c77c81c6ddc5852df9
Noor <noorshyk20@gmail.com>
2023-08-24 08:38:37 +0500

    tokenization fixed

--- CompilerCons/Tokenization.cs
+++ CompilerCons/Tokenization.cs
@@ -1,12 +1,66 @@
+using System;
+using System.Text.RegularExpressions;
 public class Tokenization
 {
 
     public Tokenization() { }
-    public char[] splittables = { ' ', ';' };
-    public char[] operators = { '+', '-', '=', '/', '*', '(', ')', '{', '}', '%','"' };
-    public string[] keyWords = { "public", "private", "static", "class", "function", "struct", "new", "return", "break", "continue", "if", "else", "while", "for", "forEach", "switch", "case"};
+    public char[] operators = { '+', '-', '=', '/', '*', '%' };
+    public char[] punctuators = { '(', ')', '{', '}', ';' };
+    public string[] keyWords = { "public", "private", "static", "class", "function", "struct", "new", "return", "break", "continue", "if", "else", "while", "for", "forEach", "switch", "case" };
     public string[] dataTypes = { "number", "string", "char", "bool", "void" };
     public string[] arrayDataTypes = { "number[]", "string[]", "char[]" };
+    string identifier = @"^[a-zA-Z_][a-zA-Z0-9_]*$";
+
+    public List<Dictionary<string, string>> makeParts(List<string> list)
+    {
+        Console.WriteLine("Entered");
+        var parts = new List<Dictionary<string, string>>();
+        foreach (var item in list)
+        {
+            Dictionary<string, string> dictionary1 = new Dictionary<string, string>();
+
+            if (operators.Contains(item[0]))
+            {
+                dictionary1["class"] = "Operator";
+            }
+            else if (punctuators.Contains(item[0]))
+            {
+                dictionary1["class"] = "Punctuators";
+            }
+            else if (keyWords.Contains(item))
+            {
+                dictionary1["class"] = "KeyWord";
+            }
+            else if (dataTypes.Contains(item))
+            {
+                dictionary1["class"] = "DataType";
+            }
+            else if (arrayDataTypes.Contains(item))
+            {
+                dictionary1["class"] = "ArrayDataType";
+            }
+            else if (Regex.IsMatch(item, "[\"]") || Regex.IsMatch(item, "[']"))
+            {
+                dictionary1["class"] = "Quotes";
+            }
+            else if (Regex.IsMatch(item, identifier))
+            {
+                dictionary1["class"] = "Identifier";
+            }
+            else
+            {
+                dictionary1["class"] = "InvalidToken";
+            }
+
+            dictionary1["value"] = item;
+            parts.Add(dictionary1);
+        }
+        foreach (var item in parts)
+        {
+            Console.WriteLine($"Class: {item["class"]}, Value: {item["value"]}");
+        }
+        return parts;
+    }
 
     public List<string> MakeTokens(string line)
     {
@@ -20,24 +74,55 @@ public class Tokenization
         foreach (var item in line)
         {
             // Console.WriteLine($"item is {item} and is {splittables.Contains(item)}");
-            if (splittables.Contains(item))
+            // item = item.toString();
+            if (item == ' ')
             {
-                // Console.WriteLine(token);
-                tokens.Add(token);
+                if (!string.IsNullOrEmpty(token))
+                {
+                    tokens.Add(token.Trim());
+                }
                 token = string.Empty;
             }
-            else if (operators.Contains(item))
+            else if (Regex.IsMatch(item.ToString(), "[\"]") || Regex.IsMatch(item.ToString(), "[']"))
             {
-                tokens.Add(token);
+
+                if (!string.IsNullOrEmpty(token))
+                {
+                    tokens.Add(token.Trim());
+                }
                 tokens.Add(item.ToString());
                 token = string.Empty;
+
             }
-            token += item;
+            else if (operators.Contains(item) || punctuators.Contains(item))
+            {
+                if (!string.IsNullOrEmpty(token))
+                {
+                    tokens.Add(token.Trim());
+                }
+                tokens.Add(item.ToString());
+                token = string.Empty;
+
+            }
+            else
+            {
+                token += item;
+            }
 
         }
+        if (!string.IsNullOrEmpty(token))
+        {
+            tokens.Add(token);
+        }
         Console.WriteLine(tokens.Count);
+        foreach (var item in tokens)
+        {
+            Console.WriteLine(item);
+        }
+        makeParts(tokens);
         return tokens;
     }
-
-
 }
+
+
+
